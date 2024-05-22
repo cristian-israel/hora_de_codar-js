1d1aff44cd1d0a385b031fbc67ab0e25dcbb0433
cristian-israel <israel.dev@icloud.com>
2024-05-21 23:55:59 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,9 @@
+console.clear();
+
+valor_1 = "6";
+valor_2 = 2;
+
+console.log(`valor_1: ${typeof valor_1}\nvalor_2: ${typeof valor_2}\n`);
+
+console.log(valor_1 - valor_2);
+console.log(valor_1 + valor_2);
